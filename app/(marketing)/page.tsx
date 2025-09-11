68c8aa6235b42beb4f37612d26a35d0c9f7c2392
Andres Soria <andres.zov@gmail.com>
2025-09-11 15:41:02 -0600

    Refactor MarketingPage component structure and remove Button import

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,9 +1,9 @@
-import { Button } from '@/components/ui/button';
-
-export default function Home() {
+const MarketingPage = () => {
   return (
-    <div>
-      <Button variant={'default'}>Home</Button>
+    <div className="min-h-full flex flex-col">
+      <div className="flex flex-col items-center justify-center md:justify-start text-center gap-y-8 flex-1 px-6 pb-10"></div>
     </div>
   );
-}
+};
+
+export default MarketingPage;
