64ad7313cd319520493fcc4c5385317640d14204
Andres Soria <andres.zov@gmail.com>
2025-09-15 11:12:51 -0600

    Update Heading component with improved styling and copy

--- app/(marketing)/_components/heading.tsx
+++ app/(marketing)/_components/heading.tsx
@@ -1,12 +1,23 @@
 'use client';
 
+import { Button } from '@/components/ui/button';
+import { ArrowRight } from 'lucide-react';
+
 export const Heading = () => {
   return (
     <div className="max-w-3xl space-y-4">
-      <h1>
+      <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold ">
         Your Ideas, Documents & Plains. Unified. Welcome to{' '}
-        <span className="underline">Notion</span>
+        <span className="underline">Not-ion</span>
       </h1>
+      <h3 className="text-base sm:text-xl md:text-2xl font-medium">
+        Not-ion is the connencted workspace where <br /> better, faster work
+        happens
+      </h3>
+      <Button>
+        Get Started
+        <ArrowRight className="h-4 w-4 ml-2" />
+      </Button>
     </div>
   );
 };
