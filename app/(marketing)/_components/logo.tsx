f50471cb5cfa43ed62d7cf9f3ee9be50febc9165
Andres Soria <andres.zov@gmail.com>
2025-09-18 11:32:29 -0600

    Create Logo component

---
+++ app/(marketing)/_components/logo.tsx
@@ -0,0 +1,17 @@
+import { cn } from '@/lib/utils';
+import { Poppins } from 'next/font/google';
+import Image from 'next/image';
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['400', '600'],
+});
+
+export const Logo = () => {
+  return (
+    <div className="hidden md:flex items-center gap-x-2">
+      <Image src={'/logo.svg'} height="40" width="40" alt="Logo" />
+      <p className={cn(font.className, 'font-semibold')}> Not-ion</p>
+    </div>
+  );
+};
